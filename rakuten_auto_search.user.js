49d6473c5c1766828497f989e1a722653e267859
cojicojigray <cojicoji_gray@yahoo.co.jp>
2020-08-01 03:46:29 +0900

    Update for Ver1.5
    検索ワードの追加
    検索ワードの配列を.pushで追加
    次の検索ワードをランダムに決定
    次の検索ワードにリンクを生成

--- rakuten_auto_search.user.js
+++ rakuten_auto_search.user.js
@@ -1,75 +1,100 @@
 // ==UserScript==
-// @name           RakutenAutoSearch
-// @namespace      RakutenAutoSearch
-// @description    一定時間毎に楽天サーチのキーワードを切り替えて検索する
-// @include        http://websearch.rakuten.co.jp/*
-// @version        1.4
+// @name         RakutenAutoSearch
+// @namespace    RakutenAutoSearch
+// @version      1.5
+// @description  一定時間毎に楽天サーチのキーワードを切り替えて検索する
+// @author       You
+// @match        https://websearch.rakuten.co.jp/*
+// @grant        none
 // ==/UserScript==
+
+(function() {
+    'use strict';
 
 try{
 var aryKeyWord = new Array();
-aryKeyWord[0] = 'オールクーポンジャパン';
-aryKeyWord[1] = 'iphone';
-aryKeyWord[2] = 'apple';
-aryKeyWord[3] = 'カート';
-aryKeyWord[4] = 'amazon';
-aryKeyWord[5] = 'google';
-aryKeyWord[6] = 'twitter';
-aryKeyWord[7] = 'readitlater';
-aryKeyWord[8] = 'tumblr';
-aryKeyWord[9] = 'facebook';
-aryKeyWord[10] = 'mixi';
-aryKeyWord[11] = 'セコムの食';
-aryKeyWord[12] = 'superfly';
-aryKeyWord[13] = 'perfume';
-aryKeyWord[14] = 'mac mini';
-aryKeyWord[15] = 'evernote';
-aryKeyWord[16] = '情報デザイン';
-aryKeyWord[17] = 'ライフハック';
-aryKeyWord[18] = 'ブレインストーミング';
-aryKeyWord[19] = 'マインドマップ';
-aryKeyWord[20] = 'piku';
-aryKeyWord[21] = 'はてな';
-aryKeyWord[22] = 'qpod';
-aryKeyWord[23] = '任天堂';
-aryKeyWord[24] = 'HTML5';
-aryKeyWord[25] = 'CSS3';
-aryKeyWord[26] = 'YAHOO';
-aryKeyWord[27] = 'プレゼント';
-aryKeyWord[28] = 'クーポン';
-aryKeyWord[29] = 'オークション';
-aryKeyWord[30] = '訳あり';
-aryKeyWord[31] = 'Javascript';
-aryKeyWord[32] = 'Ajax';
-aryKeyWord[33] = 'jquery';
-aryKeyWord[34] = 'wordpress';
-aryKeyWord[35] = 'movabletype';
-aryKeyWord[36] = 'ポイント';
-aryKeyWord[37] = 'ちはやふる';
-aryKeyWord[38] = 'ワンピース';
-aryKeyWord[39] = '料理';
-aryKeyWord[40] = 'ポケモン';
-aryKeyWord[41] = '文房具';
-aryKeyWord[42] = 'TV番組表';
-aryKeyWord[43] = 'プロジェクト';
-aryKeyWord[44] = 'AppleScript';
-aryKeyWord[45] = '自転車';
-aryKeyWord[46] = 'Foursquare';
-aryKeyWord[47] = 'ニコニコ';
+aryKeyWord.push('クーポン');
+aryKeyWord.push('iphone');
+aryKeyWord.push('apple');
+aryKeyWord.push('カート');
+aryKeyWord.push('amazon');
+aryKeyWord.push('google');
+aryKeyWord.push('twitter');
+aryKeyWord.push('pocket');
+aryKeyWord.push('tumblr');
+aryKeyWord.push('facebook');
+aryKeyWord.push('slack');
+aryKeyWord.push('セコムの食');
+aryKeyWord.push('superfly');
+aryKeyWord.push('perfume');
+aryKeyWord.push('mac mini');
+aryKeyWord.push('evernote');
+aryKeyWord.push('情報デザイン');
+aryKeyWord.push('ライフハック');
+aryKeyWord.push('ブレインストーミング');
+aryKeyWord.push('マインドマップ');
+aryKeyWord.push('Youtube');
+aryKeyWord.push('はてな');
+aryKeyWord.push('AWS');
+aryKeyWord.push('任天堂');
+aryKeyWord.push('HTML5');
+aryKeyWord.push('CSS3');
+aryKeyWord.push('YAHOO');
+aryKeyWord.push('プレゼント');
+aryKeyWord.push('割引チケット');
+aryKeyWord.push('オークション');
+aryKeyWord.push('訳あり');
+aryKeyWord.push('Javascript');
+aryKeyWord.push('Ajax');
+aryKeyWord.push('jquery');
+aryKeyWord.push('wordpress');
+aryKeyWord.push('movabletype');
+aryKeyWord.push('ポイント');
+aryKeyWord.push('ちはやふる');
+aryKeyWord.push('ワンピース');
+aryKeyWord.push('料理');
+aryKeyWord.push('ポケモン');
+aryKeyWord.push('文房具');
+aryKeyWord.push('TV番組表');
+aryKeyWord.push('プロジェクト');
+aryKeyWord.push('AppleScript');
+aryKeyWord.push('自転車');
+aryKeyWord.push('コロナウィルス');
+aryKeyWord.push('あまちゃん');
+aryKeyWord.push('屋久島');
+aryKeyWord.push('静岡県');
+aryKeyWord.push('岩手県');
+aryKeyWord.push('旅行');
+aryKeyWord.push('登山');
+aryKeyWord.push('どうぶつの森');
+aryKeyWord.push('zoom');
+aryKeyWord.push('Teams');
+aryKeyWord.push('コミュニティツール');
+aryKeyWord.push('RPA');
+aryKeyWord.push('クイズ');
+aryKeyWord.push('テニス');
+aryKeyWord.push('マインクラフト');
+aryKeyWord.push('ニュース');
+aryKeyWord.push('メタルクラフト');
+aryKeyWord.push('Kubernetes');
+aryKeyWord.push('Azure');
 
-var intInterval =10 * 60 * 1000;
+
+
+
+var intInterval = (5+Math.floor(Math.random()*5)) * 60 * 1000;
 var strSearchURL = "http://websearch.rakuten.co.jp/Web?qt=";
 var strSearchPram = "&col=OW&svx=101102";
 
 var dtNow = new Date();
 
-var strKeyWord = aryKeyWord[Number(dtNow.getHours()*2+Math.floor(dtNow.getMinutes()/10))];
-//var strKeyWord = aryKeyWord[Math.floor( Math.random() * 23 )];
+//var strKeyWord = aryKeyWord[Number(dtNow.getHours()*2+Math.floor(dtNow.getMinutes()/10))];
+var strKeyWord = aryKeyWord[Math.floor( Math.random() * aryKeyWord.length )];
 //var strKeyWord = aryKeyWord[12];
 
 var tidRaketenSearch=setInterval(function(){document.location.href = strSearchURL + strKeyWord + strSearchPram;},intInterval);
 
-strResurtMessage = "<div style='font-size:9px;'>RakutenAutoSearch:　次は「" + strKeyWord + "」で検索　<a href='" + strSearchURL + strKeyWord + strSearchPram + ">いますぐ検索する</a>　("+ dtNow + ")";
+var strResurtMessage = "<div style='font-size:9px;'>RakutenAutoSearch:　次は「<a id='RAS' href='" + strSearchURL + strKeyWord + strSearchPram + "'>" + strKeyWord + "</a>」で検索";
 
 } catch(e) {
 strResurtMessage = "<div style='font-size:9px;color:red;'>RakutenAutoSearch:"+e+"</div>";
@@ -83,6 +108,10 @@ document.body.insertBefore(debugDiv,document.body.firstChild);
 
 // 楽天サーチの画面を維持させるためにリンクは全て別タブで表示させる
 var aryAtag = document.getElementsByTagName("a");
+var i;
 for(i=0;i<aryAtag.length;i++){
-aryAtag[i].target = "_blank";
+    if(aryAtag[i].id != "RAS"){
+        aryAtag[i].target = "_blank";
+    }
 }
+})();
